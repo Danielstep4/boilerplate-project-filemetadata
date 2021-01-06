8a1953d086199cf22cf60f67c5008db8ca9b2b6e
Danielstep4 <63352307+Danielstep4@users.noreply.github.com>
2021-01-06 11:40:43 +0200

    Completed Challange
    Completed Challange

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const bodyParser = require("body-parser");
+const multer  = require('multer');
+const upload = multer();
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
-
+const app = express();
+app.use(bodyParser.json());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
+app.post('/api/fileanalyse', upload.any(), function (req, res) {
+  const response = {
+    name: req.files[0].originalname,
+    type: req.files[0].mimetype,
+    size: req.files[0].size
+  }
+  res.json(response)
+})
 
 
 const port = process.env.PORT || 3000;
